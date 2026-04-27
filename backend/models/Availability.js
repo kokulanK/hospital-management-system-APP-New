c1acc2617103de9c1af2c7f655446f464f508fc8
kokulan kugathasan <kokulankugathasan2003@gmail.com>
2026-04-27 23:36:06 +0530

    Initial commit: add app and backend

--- backend/models/Availability.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const availabilitySchema = new mongoose.Schema({
-  doctor: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  startTime: { type: Date, required: true },
-  endTime: { type: Date, required: true },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Availability', availabilitySchema);
--- backend/routes/patientRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { protect, authorize } = require('../middleware/authMiddleware');
-const { getPersonalizedTip } = require('../controllers/tipController');
-
-const router = express.Router();
-
-router.get('/tip', protect, authorize('patient'), getPersonalizedTip);
-
-module.exports = router;
